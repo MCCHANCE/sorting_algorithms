616cd1ac90cd4f5b7bdff00cadcb2e25ea31e2e4
MCCHANCE <unwananyong2021@gmail.com>
2023-07-25 02:05:03 -0700

    101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,11 +1,5 @@
 #include "sort.h"
 
-
-void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
-void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
-void cocktail_sort_list(listint_t **list);
-
-
 /**
  * swap_node_ahead - Swapping a node in a listint_t doubly-linked list
  *                   list of int with the node ahead of it.
